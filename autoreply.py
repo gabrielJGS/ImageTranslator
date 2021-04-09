c7d24c11c878df530f29721f4c50dd167e7d459e
Gabriel José <gabrielftjose@gmail.com>
2021-04-09 18:52:06 -0300

    Tratar nomedia

--- autoreply.py
+++ autoreply.py
@@ -12,35 +12,46 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 langtypes = langs()
 
+
 def check_mentions(api, since_id):
     logger.info("Retrieving mentions")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline,
                                since_id=since_id).items():
-        tweetText=tweet.text.lower()
+        tweetText = tweet.text.lower()
         selectLang = 0
         for lan in langtypes:
-            selectLang=tweetText.find("-"+lan)
-            if(selectLang>0):
-                selLang=tweetText[selectLang+1:selectLang+1+len(lan)]
+            selectLang = tweetText.find("-"+lan)
+            if(selectLang > 0):
+                selLang = tweetText[selectLang+1:selectLang+1+len(lan)]
                 break
-        if(selectLang<0):
+        if(selectLang < 0):
             logger.info("Invalid tweet lang")
             api.update_status(
-                    status="Você selecionou uma linguagem inválida, leia o fixado e tente novamente\n"+str(datetime.now()),
-                    in_reply_to_status_id=tweet.id,
-                )
+                status="Você selecionou uma linguagem inválida, leia o fixado e tente novamente\n" +
+                str(datetime.now()),
+                in_reply_to_status_id=tweet.id,
+            )
         else:
-            for med in tweet.entities['media']:
-                message = translate(med['media_url'], selLang)
-                message = message[0:280]
-                new_since_id = max(tweet.id, new_since_id)
-                if tweet.in_reply_to_status_id is not None:
-                    continue
-                logger.info("Answering to "+tweet.user.name)
+            if('media' in tweet.entities):
+                for med in tweet.entities['media']:
+                    message = translate(med['media_url'], selLang)
+                    message = message[0:280]
+                    new_since_id = max(tweet.id, new_since_id)
+                    if tweet.in_reply_to_status_id is not None:
+                        continue
+                    logger.info("Answering to "+tweet.user.name)
+
+                    api.update_status(
+                        status=message,
+                        in_reply_to_status_id=tweet.id,
+                    )
+            else:
+                logger.info("NoMedia")
 
                 api.update_status(
-                    status=message,
+                    status="Você não anexou nenhum arquivo!\n" +
+                    str(datetime.now()),
                     in_reply_to_status_id=tweet.id,
                 )
     return new_since_id
