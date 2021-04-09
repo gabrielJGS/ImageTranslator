bed85f559e32fe1e55eaa6d94fe191273269fe2d
Gabriel José <gabrielftjose@gmail.com>
2021-04-09 19:39:04 -0300

    Salvar lastid

--- autoreply.py
+++ autoreply.py
@@ -16,8 +16,7 @@ langtypes = langs()
 def check_mentions(api, since_id):
     logger.info("Retrieving mentions")
     new_since_id = since_id
-    for tweet in tweepy.Cursor(api.mentions_timeline,
-                               since_id=since_id).items():
+    for tweet in tweepy.Cursor(api.mentions_timeline,since_id=since_id).items():
         tweetText = tweet.text.lower()
         selectLang = 0
         for lan in langtypes:
@@ -37,7 +36,7 @@ def check_mentions(api, since_id):
                 for med in tweet.entities['media']:
                     message = translate(med['media_url'], selLang)
                     message = message[0:280]
-                    new_since_id = max(tweet.id, new_since_id)
+                    
                     if tweet.in_reply_to_status_id is not None:
                         continue
                     logger.info("Answering to "+tweet.user.name)
@@ -47,21 +46,29 @@ def check_mentions(api, since_id):
                         in_reply_to_status_id=tweet.id,
                     )
             else:
-                logger.info("NoMedia")
+                logger.info("Answering noMedia")
 
                 api.update_status(
                     status="Você não anexou nenhum arquivo!\n" +
                     str(datetime.now()),
                     in_reply_to_status_id=tweet.id,
                 )
+        new_since_id = max(tweet.id, new_since_id)
+        print(new_since_id)
     return new_since_id
 
 
 def main():
+    # Abre arquivo
+    idfile = open('lastid.txt', 'r+')
+    since_id = int(idfile.read())
+
     api = create_api()
-    since_id = 1
     while True:
         since_id = check_mentions(api, since_id)
+        idfile.seek(0)
+        idfile.write(str(since_id))
+        idfile.truncate()
         logger.info("Waiting...")
         time.sleep(60)
 
